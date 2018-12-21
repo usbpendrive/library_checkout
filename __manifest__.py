648d0d736713b561b8dad8def49a9badcd596c45
usbpendrive <ferry.fernando@gmail.com>
2018-12-21 13:18:13 +0700

    Library checkout module initialize.

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Library Book Borrowing',
+    'description': 'Members can borrow books from the library.',
+    'author': 'Fernando',
+    'license': 'LGPL-3',
+    'depends': [
+        'library_member',
+        'mail',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/library_menu.xml',
+        'views/checkout_view.xml',
+        'data/library_checkout_stage.xml',
+        'wizard/checkout_mass_message_wizard.xml',
+    ],
+}
---
+++ wizard/checkout_mass_message.py
@@ -0,0 +1,48 @@
+from odoo import api, exceptions, fields, models
+
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class CheckoutMassMessage(models.TransientModel):
+    _name = 'library.checkout.massmessage'
+    _description = 'Send message to Borrowers'
+
+    checkout_ids = fields.Many2many('library.checkout', string='Checkouts')
+    message_subject = fields.Char()
+    message_body = fields.Html()
+
+    @api.model
+    def default_get(self, fields_list):
+        defaults = super().default_get(fields_list)
+        checkout_ids = self.env.context.get('active_ids')
+        defaults['checkout_ids'] = checkout_ids
+        return defaults
+
+    @api.multi
+    def button_send(self):
+        import pudb; pu.db
+        self.ensure_one()
+
+        if not self.checkout_ids:
+            raise exceptions.UserError('Select at least one Checkout to send messages to.')
+        if not self.message_body:
+            raise exceptions.UserError('Write a message body to send.')
+
+        for checkout in self.checkout_ids:
+            checkout.message_post(
+                body=self.message_body,
+                subject=self.message_subject,
+                subtype='mail.mt_comment',
+            )
+            _logger.debug(
+                'Message on %d to followers: %s',
+                checkout.id,
+                checkout.message_follower_ids)
+
+        _logger.info(
+            'Posted %d messages to Checkouts: %s',
+            len(self.checkout_ids),
+            self.checkout_ids.ids,
+        )
+        return True
